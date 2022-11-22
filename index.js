4136c2dd9fefc7dc02eb7b4d050eada595ca74d8
Teresa <tenjerim@gmail.com>
2022-11-22 21:19:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1= 31
+const num2= 2
+const num3= 24
+const num4= 5
+let multiply = num1 * num2
+multiply
+
+// random code
+const random = Math.floor(Math.random() * 6 ) +2
+random
+
+//calculating modulus remainder
+const mod = num3 % num4
+mod
+
+//calculate max
+const max = Math.max(15, 20, 11, 3, 5)
